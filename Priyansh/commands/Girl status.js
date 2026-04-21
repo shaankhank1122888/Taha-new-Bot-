7eb4f666f34c867790529698fe41d6be5edcc4ef
Taha khan <tahachandia84@gmail.com>
2026-04-21 20:14:52 +0500

    Update Girl status.js

--- Priyansh/commands/Girl status.js
+++ Priyansh/commands/Girl status.js
@@ -18,7 +18,7 @@ module.exports.run = async({api,event,args,client,Users,Threads,__GLOBAL,Currenc
 const axios = global.nodemodule["axios"];
 const request = global.nodemodule["request"];
 const fs = global.nodemodule["fs-extra"];
-   var hi = ["♥️𝑺𝑯𝑨𝑨𝑵-𝑲𝑯𝑨𝑵♥️"];
+   var hi = ["♥️𝐓𝐀𝐇𝐀 𝐊𝐇𝐀𝐍♥️"];
   var know = hi[Math.floor(Math.random() * hi.length)];
   var link = [
 
